0d7b0d659fc78c84548052e555a9ea2299a3a07b
G.Pavan Sai Sriram <93752325+thepavansai@users.noreply.github.com>
2026-04-27 12:53:52 +0530

    Initialize the application

---
+++ src/main/java/com/phantom/familycashcards/FamilyCashCardsApplication.java
@@ -0,0 +1,13 @@
+package com.phantom.familycashcards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyCashCardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FamilyCashCardsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phantom/familycashcards/FamilyCashCardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phantom.familycashcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyCashCardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
